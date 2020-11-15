b4316b27b183fb3cb2dffef5990cc51c19b97c47
Asier Aduriz <aaduriz@vistaprint.com>
2020-11-15 09:35:11 +0100

    Second mars rover kata

---
+++ MarsRoverKata2/MarsRoverKata/MarsRover.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarsRoverKata
+{
+    public class MarsRover
+    {
+        private readonly List<string> PERMITTED_DIRECTIONS = new List<string>()
+        {
+            "N",
+            "E",
+            "S",
+            "W"
+        };
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public string D { get; private set; }
+
+        public MarsRover(int x, int y, string direction)
+        {
+            SetStartingCoordinates(x, y);
+            SetStartingDirection(direction);
+        }
+
+        public void SetStartingCoordinates(int x, int y)
+        {
+            if (x < 0 || y < 0)
+            {
+                throw new ArgumentException("Starting X and/or Y coords are invalid");
+            }
+
+            X = x;
+            Y = y;
+        }
+
+        private void SetStartingDirection(string direction)
+        {
+            if (PERMITTED_DIRECTIONS.Contains(direction))
+            {
+                this.D = direction;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid starting direction");
+            }
+        }
+
+        public void Move(string[] commands)
+        {
+            foreach (string command in commands)
+            {
+                if (IsCommandValid(command))
+                {
+                    switch (D)
+                    {
+                        case "N":
+                            Y += (command.Equals("F")) ? 1 : -1;
+                            break;
+                        case "E":
+                            X += command.Equals("F") ? 1 : -1;
+                            break;
+                        case "S":
+                            Y -= command.Equals("F") ? 1 : -1;
+                            break;
+                        case "W":
+                            X -= command.Equals("F") ? 1 : -1;
+                            break;
+
+                    }
+                }
+            }
+        }
+
+        private bool IsCommandValid(string command)
+        {
+            return command.Equals("F") || command.Equals("B");
+        }
+    }
+}
---
+++ MarsRoverKata2/MarsRoverKataTest/MarsRoverShould.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using MarsRoverKata;
+using System;
+
+namespace MarsRoverKataTest
+{
+
+    [TestFixture]
+    public class MarsRoverShould
+    {
+        [TestCase(0, 0, "N")]
+        [TestCase(0, 0, "E")]
+        [TestCase(0, 0, "S")]
+        [TestCase(0, 0, "W")]
+        [TestCase(2, 1, "N")]
+        [TestCase(0, 4, "N")]
+        public void Receive_ValidStartingData(int x, int y, string direction)
+        {
+            MarsRover marsRover = new MarsRover(x, y, direction);
+
+            Assert.AreEqual(marsRover.X, x);
+            Assert.AreEqual(marsRover.Y, y);
+            Assert.AreEqual(marsRover.D, direction);
+        }
+
+        [Test]
+        public void Move_AccordingToCommands()
+        {
+            int x = 5;
+            int y = 5;
+            string direction = "N";
+            int forward_amount = 3;
+            int backward_amount = 1;
+
+            MarsRover marsRover = new MarsRover(x, y, direction);
+            string[] commands = { "F", "B", "F", "F" };
+
+            marsRover.Move(commands);
+
+            Assert.AreEqual(y + (forward_amount - backward_amount), marsRover.Y);
+
+        }
+
+    }
+}
